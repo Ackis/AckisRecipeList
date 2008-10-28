74a2fea456a4bf95efda7a3b3f53f075f429f3c7
Ackis <Ackis@dummy.example.com>
2008-10-28 22:32:53 +0000

    Minor Dataminer tweak

--- ARLDataminer.rb
+++ ARLDataminer.rb
@@ -1140,46 +1140,15 @@ if $debug
 		27033 => {:id => 12},
 		27032 => {:id => 7, :type => 1}
 		}
-	#create_profession_db("./RecipeDB/ARL-FirstAid.lua","First Aid",recipes,maps,"InitFirstAid",firstaid,[30021],faspecaillist,[45545, 45546, 51801])
+	create_profession_db("./RecipeDB/ARL-FirstAid.lua","First Aid",recipes,maps,"InitFirstAid",firstaid,[30021],faspecaillist,[45545, 45546, 51801])
 
-	eng = recipes.get_engineering_list
-	engspecaillist = {
-		21940 => {:id => 7, :type => 1},
-		26416 => {:id => 7, :type => 2},
-		26417 => {:id => 7, :type => 2},
-		26418 => {:id => 7, :type => 2},
-		26420 => {:id => 7, :type => 2},
-		26421 => {:id => 7, :type => 2},
-		26422 => {:id => 7, :type => 2},
-		26423 => {:id => 7, :type => 2},
-		26424 => {:id => 7, :type => 2},
-		26425 => {:id => 7, :type => 2},
-		26426 => {:id => 7, :type => 2},
-		26427 => {:id => 7, :type => 2},
-		26428 => {:id => 7, :type => 2},
-		26442 => {:id => 7, :type => 2},
-		26443 => {:id => 7, :type => 2},
-		40274 => {:id => "class", :type => [25, 29]},
-		41311 => {:id => "class", :type => [25]},
-		41312 => {:id => "class", :type => [25, 29]},
-		41314 => {:id => "class", :type => [23, 27]},
-		41315 => {:id => "class", :type => [27]},
-		41316 => {:id => "class", :type => [27]},
-		41317 => {:id => "class", :type => [22, 28]},
-		41318 => {:id => "class", :type => [22]},
-		41319 => {:id => "class", :type => [22]},
-		41320 => {:id => "class", :type => [24, 26, 29]},
-		41321 => {:id => "class", :type => [26]},
-		}
-	create_profession_db("./RecipeDB/ARL-Engineer.lua","Engineering",recipes,maps,"InitEngineering",eng,[30343,30342,30349,30561,30549,12722,12720,12900,12719,12904],engspecaillist,[53280,53281])
+	create_lookup_db("./RecipeDB/ARL-Trainer.lua","Trainer","TrainerDB","InitTrainer",$trainers,maps,[])
 
-	#create_lookup_db("./RecipeDB/ARL-Trainer.lua","Trainer","TrainerDB","InitTrainer",$trainers,maps,[])
+	create_lookup_db("./RecipeDB/ARL-Vendor.lua","Vendor","VendorDB","InitVendor",$vendors,maps,[])
 
-	#create_lookup_db("./RecipeDB/ARL-Vendor.lua","Vendor","VendorDB","InitVendor",$vendors,maps,[])
+	create_lookup_db("./RecipeDB/ARL-Mob.lua","Monster","MobDB","InitMob",$monsters,maps,[])
 
-	#create_lookup_db("./RecipeDB/ARL-Mob.lua","Monster","MobDB","InitMob",$monsters,maps,[])
-
-	#create_lookup_db("./RecipeDB/ARL-Quest.lua","Quest","QuestDB","InitQuest",$quests,maps,[])
+	create_lookup_db("./RecipeDB/ARL-Quest.lua","Quest","QuestDB","InitQuest",$quests,maps,[])
 
 else
 
