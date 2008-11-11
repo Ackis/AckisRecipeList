a39327fd11809984dcd9d87ff2a7aee3916cfb8e
rabbit <rabbit@dummy.example.com>
2008-11-11 09:01:12 +0000

    Fix warnings about missing babble libraries.

--- AckisRecipeList.lua
+++ AckisRecipeList.lua
@@ -33,28 +33,28 @@ local addon		= LibStub("AceAddon-3.0"):GetAddon(MODNAME)
 -- Lets check to see if we have the needed libraries loaded (these are manditory to run)
 if (not LibStub:GetLibrary("LibBabble-Faction-3.0", true)) then
 
-	self:Print("LibBabble-Faction-3.0 not loaded.  Addon cannot run.")
+	AckisRecipeList:Print("LibBabble-Faction-3.0 not loaded.  Addon cannot run.")
 	return
 
 end
 
 if (not LibStub:GetLibrary("LibBabble-Zone-3.0", true)) then
 
-	self:Print("LibBabble-Zone-3.0 not loaded.  Addon cannot run.")
+	AckisRecipeList:Print("LibBabble-Zone-3.0 not loaded.  Addon cannot run.")
 	return
 
 end
 
 if (not LibStub:GetLibrary("LibBabble-Boss-3.0", true)) then
 
-	self:Print("LibBabble-Boss-3.0 not loaded.  Addon cannot run.")
+	AckisRecipeList:Print("LibBabble-Boss-3.0 not loaded.  Addon cannot run.")
 	return
 
 end
 
 if (not LibStub:GetLibrary("AceLocale-3.0", true)) then
 
-	self:Print("AceLocale-3.0 not loaded.  Addon cannot run.")
+	AckisRecipeList:Print("AceLocale-3.0 not loaded.  Addon cannot run.")
 	return
 
 end
