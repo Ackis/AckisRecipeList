94bb1f8989b3a80793cc9aa1a350c278bd8ea755
ackis <ackis@dummy.example.com>
2009-01-13 20:49:31 +0000

    Convert all non-enUS locals over to using the localization application.

--- Locals/testenUS.lua
+++ Locals/testenUS.lua
@@ -45,4 +45,4 @@ local L = LibStub("AceLocale-3.0"):NewLocale("MyAddon", "enUS", true, debug)
 
 if not L then return end
 
---@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="english", escape-non-ascii="false")@
+--@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="english", escape-non-ascii=false, same-key-is-true=true)@
