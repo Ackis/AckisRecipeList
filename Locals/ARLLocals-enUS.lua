6e97fc3ebe0246819f88586c82f14356f4b11785
ackis <ackis@dummy.example.com>
2009-01-20 23:17:05 +0000

    Make more docs.

--- Locals/ARLLocals-enUS.lua
+++ Locals/ARLLocals-enUS.lua
@@ -20,20 +20,6 @@ additions or changes.
 ****************************************************************************************
 ]]--
 
---[[
-Locale schema
-	* single and double words are just fine, anything longer should have a tag
-	* tag name rules:
-		* tags should be ALL_CAPITAL_LETTERS with words separated with _'s
-		* descriptions are typically used in tooltips and header in sections
-		* short descriptions will end in _DESC
-		* long descriptions will end in _LONG
-	* don't include the object being described in the name (no _TOGGLE or _TT) since it
-	  can be used several times to describe different objects (in theory)
-	* group related elements together, either by their location in the GUI/config,
-	  or by purpose (Weapon categories, etc)
-]]--
-
 local L = LibStub("AceLocale-3.0"):NewLocale("Ackis Recipe List", "enUS", true)
 
 if not L then return end
