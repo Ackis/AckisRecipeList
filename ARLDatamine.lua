72664f64babb2f7f9f7dc2e9dfa081f0545ebc66
ackis <ackis@dummy.example.com>
2009-04-18 04:53:26 +0000

    fix locale error w/ dm

--- ARLDatamine.lua
+++ ARLDatamine.lua
@@ -19,6 +19,8 @@ Project version: @project-version@
 local MODNAME			= "Ackis Recipe List"
 local addon				= LibStub("AceAddon-3.0"):GetAddon(MODNAME)
 
+local L					= LibStub("AceLocale-3.0"):GetLocale(MODNAME)
+
 local GetNumTrainerServices = GetNumTrainerServices
 local GetTrainerServiceInfo = GetTrainerServiceInfo
 local IsTradeskillTrainer = IsTradeskillTrainer
