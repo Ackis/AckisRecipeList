f89d05f9d4e1788591f8611720ce957336bd7c88
ackis <ackis@dummy.example.com>
2009-05-13 21:17:51 +0000

    Add skill up info, expansion info, and remove recipe counts (only return one number instead of increasing it for each recipe) for First Aid, Runeforging.

--- RecipeDB/ARL-FirstAid.lua
+++ RecipeDB/ARL-FirstAid.lua
@@ -15,12 +15,6 @@ Project version: @project-version@
 
 ************************************************************************
 
-Format:
-
-	self:addTradeSkill(RecipeDB, Spell ID, Skill Level, Item ID, Rarity, Specialty)
-
-************************************************************************
-
 ]]--
 
 local MODNAME			= "Ackis Recipe List"
@@ -30,110 +24,91 @@ local L					= LibStub("AceLocale-3.0"):GetLocale(MODNAME)
 
 function addon:InitFirstAid(RecipeDB)
 
-	local recipecount = 0
-
 	-- Linen Bandage -- 3275
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,3275,1,1251,1,746)
+	self:addTradeSkill(RecipeDB,3275,1,1251,1,746,0,1,30,45,60)
 	self:addTradeFlags(RecipeDB,3275,1,2,3,21,22,23,24,25,26,27,28,29,30,36,41)
 	self:addTradeAcquire(RecipeDB,3275,8,8)
 
 	-- Heavy Linen Bandage -- 3276
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,3276,40,2581,1,746)
+	self:addTradeSkill(RecipeDB,3276,40,2581,1,746,0,40,50,75,100)
 	self:addTradeFlags(RecipeDB,3276,1,2,3,21,22,23,24,25,26,27,28,29,30,36,41)
 	self:addTradeAcquire(RecipeDB,3276,1,2326,1,2327,1,2329,1,2798,1,3181,1,3373,1,4211,1,4591,1,5150,1,5759,1,5939,1,5943,1,6094,1,16272,1,16662,1,16731,1,17214,1,17424,1,19184,1,19478,1,22477,1,33589,1,28706,1,33621)
 
 	-- Wool Bandage -- 3277
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,3277,80,3530,1,746)
+	self:addTradeSkill(RecipeDB,3277,80,3530,1,746,0,0,80,115,150)
 	self:addTradeFlags(RecipeDB,3277,1,2,3,21,22,23,24,25,26,27,28,29,30,36,41)
 	self:addTradeAcquire(RecipeDB,3277,1,2326,1,2327,1,2329,1,2798,1,3181,1,3373,1,4211,1,4591,1,5150,1,5759,1,5939,1,5943,1,6094,1,16272,1,16662,1,16731,1,17214,1,17424,1,19184,1,19478,1,22477,1,33589,1,28706,1,33621)
 
 	-- Heavy Wool Bandage -- 3278
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,3278,115,3531,1,746)
+	self:addTradeSkill(RecipeDB,3278,115,3531,1,746,0,0,115,150,185)
 	self:addTradeFlags(RecipeDB,3278,1,2,3,21,22,23,24,25,26,27,28,29,30,36,41)
 	self:addTradeAcquire(RecipeDB,3278,1,2326,1,2327,1,2329,1,2798,1,3181,1,3373,1,4211,1,4591,1,5150,1,5759,1,5939,1,5943,1,6094,1,16272,1,16662,1,16731,1,17214,1,17424,1,19184,1,19478,1,22477,1,33589,1,28706,1,33621)
 
 	-- Silk Bandage -- 7928
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,7928,150,6450,1,746)
+	self:addTradeSkill(RecipeDB,7928,150,6450,1,746,0,0,150,180,210)
 	self:addTradeFlags(RecipeDB,7928,1,2,3,21,22,23,24,25,26,27,28,29,30,36,41)
 	self:addTradeAcquire(RecipeDB,7928,1,2326,1,2327,1,2329,1,2798,1,3181,1,3373,1,4211,1,4591,1,5150,1,5759,1,5939,1,5943,1,6094,1,16272,1,16662,1,16731,1,17214,1,17424,1,19184,1,19478,1,22477,1,33589,1,28706,1,33621)
 
 	-- Heavy Silk Bandage -- 7929
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,7929,180,6451,1,746)
+	self:addTradeSkill(RecipeDB,7929,180,6451,1,746,0,0,180,210,240)
 	self:addTradeFlags(RecipeDB,7929,1,2,3,21,22,23,24,25,26,27,28,29,30,36,40)
 	self:addTradeAcquire(RecipeDB,7929,1,2326,1,2327,1,2329,1,2798,1,3181,1,3373,1,4211,1,4591,1,5150,1,5759,1,5939,1,5943,1,6094,1,16272,1,16662,1,16731,1,17214,1,17424,1,19184,1,19478,1,22477,1,33589,1,28706,1,33621)
 
 	-- Anti-Venom -- 7934
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,7934,80,6452,1,746)
+	self:addTradeSkill(RecipeDB,7934,80,6452,1,746,0,0,80,115,150)
 	self:addTradeFlags(RecipeDB,7934,1,2,3,21,22,23,24,25,26,27,28,29,30,36,41)
 	self:addTradeAcquire(RecipeDB,7934,1,2326,1,2327,1,2329,1,2798,1,3181,1,3373,1,4211,1,4591,1,5150,1,5759,1,5939,1,5943,1,6094,1,16272,1,16662,1,16731,1,17214,1,17424,1,19184,1,19478,1,22477,1,33589,1,28706,1,33621)
 
 	-- Strong Anti-Venom -- 7935
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,7935,130,6453,2,746)
+	self:addTradeSkill(RecipeDB,7935,130,6453,2,746,0,0,130,165,200)
 	self:addTradeFlags(RecipeDB,7935,1,2,10,21,22,23,24,25,26,27,28,29,30,36,40)
 	self:addTradeAcquire(RecipeDB,7935,7,2)
 
 	-- Mageweave Bandage -- 10840
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,10840,210,8544,1,746)
+	self:addTradeSkill(RecipeDB,10840,210,8544,1,746,0,0,210,240,270)
 	self:addTradeFlags(RecipeDB,10840,1,2,3,21,22,23,24,25,26,27,28,29,30,36,40)
 	self:addTradeAcquire(RecipeDB,10840,1,2326,1,2327,1,2329,1,2798,1,3181,1,3373,1,4211,1,4591,1,5150,1,5759,1,5939,1,5943,1,6094,1,16272,1,16662,1,16731,1,17214,1,17424,1,19184,1,19478,1,22477,1,33589,1,28706,1,33621)
 
 	-- Heavy Mageweave Bandage -- 10841
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,10841,240,8545,1,746)
+	self:addTradeSkill(RecipeDB,10841,240,8545,1,746,0,0,240,270,300)
 	self:addTradeFlags(RecipeDB,10841,1,2,3,21,22,23,24,25,26,27,28,29,30,36,41)
 	self:addTradeAcquire(RecipeDB,10841,1,2326,1,2327,1,2329,1,2798,1,3181,1,3373,1,4211,1,4591,1,5150,1,5759,1,5939,1,5943,1,6094,1,16272,1,16662,1,16731,1,17214,1,17424,1,19184,1,19478,1,22477,1,33589,1,28706,1,33621)
 
 	-- Runecloth Bandage -- 18629
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,18629,260,14529,1,746)
+	self:addTradeSkill(RecipeDB,18629,260,14529,1,746,0,0,260,290,320)
 	self:addTradeFlags(RecipeDB,18629,1,2,3,21,22,23,24,25,26,27,28,29,30,36,41)
 	self:addTradeAcquire(RecipeDB,18629,1,2326,1,2327,1,2329,1,2798,1,3181,1,3373,1,4211,1,4591,1,5150,1,5759,1,5939,1,5943,1,6094,1,16272,1,16662,1,16731,1,17214,1,17424,1,19184,1,19478,1,22477,1,33589,1,28706,1,33621)
 
 	-- Heavy Runecloth Bandage -- 18630
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,18630,290,14530,1,746)
+	self:addTradeSkill(RecipeDB,18630,290,14530,1,746,0,0,290,320,350)
 	self:addTradeFlags(RecipeDB,18630,1,2,3,21,22,23,24,25,26,27,28,29,30,36,41)
 	self:addTradeAcquire(RecipeDB,18630,1,2326,1,2327,1,2329,1,2798,1,3181,1,3373,1,4211,1,4591,1,5150,1,5759,1,5939,1,5943,1,6094,1,16272,1,16662,1,16731,1,17214,1,17424,1,19184,1,19478,1,22477,1,33589,1,28706,1,33621)
 
 	-- Powerful Anti-Venom -- 23787
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,23787,300,19440,1,746)
+	self:addTradeSkill(RecipeDB,23787,300,19440,1,746,0,0,300,330,360)
 	self:addTradeFlags(RecipeDB,23787,1,2,4,21,22,23,24,25,26,27,28,29,30,36,41,96)
 	self:addTradeAcquire(RecipeDB,23787,6,529,2,10856,6,529,2,10857,6,529,2,11536)
 
 	-- Netherweave Bandage -- 27032
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,27032,300,21990,1,746)
+	self:addTradeSkill(RecipeDB,27032,300,21990,1,746,1,0,330,347,365)
 	self:addTradeFlags(RecipeDB,27032,1,2,3,4,21,22,23,24,25,26,27,28,29,30,36,40)
 	self:addTradeAcquire(RecipeDB,27032,2,18990,2,18991,2,26947,1,33589,1,28706,1,33621)
 
 	-- Heavy Netherweave Bandage -- 27033
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,27033,330,21991,1,746)
+	self:addTradeSkill(RecipeDB,27033,330,21991,1,746,1,0,360,367,375)
 	self:addTradeFlags(RecipeDB,27033,1,2,3,4,21,22,23,24,25,26,27,28,29,30,36,40)
 	self:addTradeAcquire(RecipeDB,27033,2,18990,2,18991,2,26947,1,33589,1,28706,1,33621)
 
 	-- Frostweave Bandage -- 45545
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,45545,350,34721,1,746)
+	self:addTradeSkill(RecipeDB,45545,350,34721,1,746,2,350,375,392,410)
 	self:addTradeFlags(RecipeDB,45545,1,2,3,21,22,23,24,25,26,27,28,29,30,36,41)
 	self:addTradeAcquire(RecipeDB,45545,1,23734,1,26956,1,26992,1,28706,1,29233,1,33589,1,28706,1,33621)
 
 	-- Heavy Frostweave Bandage -- 45546
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,45546,400,34722,2,746)
+	self:addTradeSkill(RecipeDB,45546,400,34722,2,746,2,0,400,430,470)
 	self:addTradeFlags(RecipeDB,45546,1,2,10,21,22,23,24,25,26,27,28,29,30,36,41)
 	self:addTradeAcquire(RecipeDB,45546,7,2)
 
-	return recipecount
+	return 17
 
 end
--- RecipeDB/ARL-Runeforge.lua
+++ RecipeDB/ARL-Runeforge.lua
@@ -15,12 +15,6 @@ Project version: @project-version@
 
 ************************************************************************
 
-Format:
-
-	self:addTradeSkill(RecipeDB, Spell ID, Skill Level, Item ID, Rarity, Specialty)
-
-************************************************************************
-
 ]]--
 
 local MODNAME			= "Ackis Recipe List"
@@ -30,62 +24,51 @@ local L					= LibStub("AceLocale-3.0"):GetLocale(MODNAME)
 
 function addon:InitRuneforging(RecipeDB)
 
-	local recipecount = 0
-
 	-- Rune of the Fallen Crusader - 53344
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,53344,1,nil,1,53428)
+	self:addTradeSkill(RecipeDB,53344,1,nil,1,53428,2,1,1,1,2)
 	self:addTradeFlags(RecipeDB,53344,1,2,3,21)
 	self:addTradeAcquire(RecipeDB,53344,1,29194,1,29196,1,29195,1,31084)
 
 	-- Rune of Swordshattering - 53323
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,53323,1,nil,1,53428)
+	self:addTradeSkill(RecipeDB,53323,1,nil,1,53428,2,1,1,1,2)
 	self:addTradeFlags(RecipeDB,53323,1,2,3,21)
 	self:addTradeAcquire(RecipeDB,53323,1,29194,1,29196,1,29195,1,31084)
 
 	-- Rune of Swordbreaking - 54446
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,54446,1,nil,1,53428)
+	self:addTradeSkill(RecipeDB,54446,1,nil,1,53428,2,1,1,1,2)
 	self:addTradeFlags(RecipeDB,54446,1,2,3,21)
 	self:addTradeAcquire(RecipeDB,54446,1,29194,1,29196,1,29195,1,31084)
 
 	-- Rune of Spellshattering - 53342
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,53342,1,nil,1,53428)
+	self:addTradeSkill(RecipeDB,53342,1,nil,1,53428,2,1,1,1,2)
 	self:addTradeFlags(RecipeDB,53342,1,2,3,21)
 	self:addTradeAcquire(RecipeDB,53342,1,29194,1,29196,1,29195,1,31084)
 
 	-- Rune of Spellbreaking - 54447
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,54447,1,nil,1,53428)
+	self:addTradeSkill(RecipeDB,54447,1,nil,1,53428,2,1,1,1,2)
 	self:addTradeFlags(RecipeDB,54447,1,2,3,21)
 	self:addTradeAcquire(RecipeDB,54447,1,29194,1,29196,1,29195,1,31084)
 
 	-- Rune of Razorice - 53343
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,53343,1,nil,1,53428)
+	self:addTradeSkill(RecipeDB,53343,1,nil,1,53428,2,1,1,1,2)
 	self:addTradeFlags(RecipeDB,53343,1,2,3,21)
 	self:addTradeAcquire(RecipeDB,53343,1,29194,1,29196,1,29195,1,31084)
 
 	-- Rune of Lichbane - 53331
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,53331,1,nil,1,53428)
+	self:addTradeSkill(RecipeDB,53331,1,nil,1,53428,2,1,1,1,2)
 	self:addTradeFlags(RecipeDB,53331,1,2,3,21)
 	self:addTradeAcquire(RecipeDB,53331,1,29194,1,29196,1,29195,1,31084)
 
 	-- Rune of Cinderglacier - 53341
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,53341,1,nil,1,53428)
+	self:addTradeSkill(RecipeDB,53341,1,nil,1,53428,2,1,1,1,2)
 	self:addTradeFlags(RecipeDB,53341,1,2,3,21)
 	self:addTradeAcquire(RecipeDB,53341,1,29194,1,29196,1,29195,1,31084)
 
 	-- Rune of the Stoneskin Gargoyle - 62158
-	recipecount = recipecount + 1
-	self:addTradeSkill(RecipeDB,62158,1,nil,1,53428)
+	self:addTradeSkill(RecipeDB,62158,1,nil,1,53428,2,1,1,1,2)
 	self:addTradeFlags(RecipeDB,62158,1,2,3,21)
 	self:addTradeAcquire(RecipeDB,62158,1,29194,1,29196,1,29195,1,31084)
 
-	return recipecount
+	return 9
 
 end
