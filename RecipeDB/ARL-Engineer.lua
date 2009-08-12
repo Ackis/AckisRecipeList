39fdd317d2d087cb39379c9542d8c0b6fe09b2fd
ackis <ackis@dummy.example.com>
2009-08-12 04:27:33 +0000

    *Removed comment in code that was causing AntiVir to screw up.  It's a fail anti-virus scanner.  --Wormhole Generator: Northrend -- 67920 causes a file to be detected as malware.  Pro.

--- RecipeDB/ARL-Engineer.lua
+++ RecipeDB/ARL-Engineer.lua
@@ -1565,7 +1565,7 @@ function addon:InitEngineering(RecipeDB)
 	self:addTradeAcquire(RecipeDB,63765,1,25277,1,26907,1,26955,1,26991,1,28697,1,33586)
 
 	-- High-powered Flashlight -- 63750
-	self:addTradeSkill(RecipeDB,63750,250,45631,1,4036,nil,2)
+	self:addTradeSkill(RecipeDB,63750,250,45631,1,4036,nil,2,250,270,280,290)
 	self:addTradeFlags(RecipeDB,63750,1,2,3,21,22,23,24,25,26,27,28,29,30,36,41,51,61)
 	self:addTradeAcquire(RecipeDB,63750,1,25277,1,26907,1,26955,1,26991,1,28697,1,16667,1,1702,1,16726,1,11017,1,33586,1,3494,
 	1,5174,1,3290,1,5518,1,33611,1,33634,1,1676,1,8736)
@@ -1580,7 +1580,7 @@ function addon:InitEngineering(RecipeDB)
 	self:addTradeFlags(RecipeDB,68067,1,2,10,21,22,23,24,25,26,27,28,29,30)
 	self:addTradeAcquire(RecipeDB,68067,7,2)
 	
-	--Wormhole Generator: Northrend -- 67920
+	-- 67920
 	self:addTradeSkill(RecipeDB,67920,450,48933,1,4036,nil,2,450,450,455,460)
 	self:addTradeFlags(RecipeDB,67920,1,2,3,21,22,23,24,25,26,27,28,29,30)
 	self:addTradeAcquire(RecipeDB,67920,1,25277)
