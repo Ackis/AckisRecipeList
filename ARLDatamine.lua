559cf0ac710f24c3db5a2261d1b03a23c597030a
ackis <ackis@dummy.example.com>
2009-08-18 17:47:00 +0000

    Add a caveat to the dataminer for engineering goggles.

--- ARLDatamine.lua
+++ ARLDatamine.lua
@@ -802,6 +802,8 @@ do
 				end
 				tinsert(output,"Trainer Acquire Scan Complete.")
 
+				tinsert(output,"If you're doing an engineering scan, there may be some goggles listed as extra.  These goggles ONLY show up for those classes who can make them, so they may be false positives.")
+
 				if ((teachflag) or (noteachflag)) then
 					self:DisplayTextDump(nil,nil,tconcat(output,"\n"))
 				end
