be0ca82315589a4ff69374cfae8876394be9ffce
ackis <ackis@dummy.example.com>
2009-08-18 20:37:01 +0000

    Add the blacksmithing recipes to the horde/ally filter implementation.  I'm sure there are some BoP quest ones that need to be modified here.

--- RecipeDB/ARL-Blacksmith.lua
+++ RecipeDB/ARL-Blacksmith.lua
@@ -2553,66 +2553,83 @@ function addon:InitBlacksmithing(RecipeDB)
 	self:addTradeFlags(RecipeDB,63182,1,2,3,21,22,23,24,25,26,27,28,29,30,36,41,51,72)
 	self:addTradeAcquire(RecipeDB,63182,1,26564,1,26904,1,26952,1,26981,1,26988,1,27034,1,28694,1,29924,1,33591)
 
-	-- Breastplate of the White Knight (Ally) -- 67091
-	self:addTradeSkill(RecipeDB,67091,450,47591,4,2018,nil,2,450,455,465,475)
-	self:addTradeFlags(RecipeDB,67091,1,6,11,21,22,23,24,25,26,27,28,29,30,36,40,52,59)
-	self:addTradeAcquire(RecipeDB,67091,8,42)
+	-- Some recipes are only availible to specific factions.
+	-- We only add the faction specific recipes if the user is part of that faction
+	local BFAC = LibStub("LibBabble-Faction-3.0"):GetLookupTable()
+	local faction = UnitFactionGroup("player")
+	-- Counter for how many faction specific recipes we have
+	local factioncount = 0
 
-	-- Breastplate of the White Knight (Horde) -- 67130
-	self:addTradeSkill(RecipeDB,67130,450,47592,4,2018,nil,2,450,455,465,475)
-	self:addTradeFlags(RecipeDB,67130,2,6,11,21,22,23,24,25,26,27,28,29,30,36,40,52,59)
-	self:addTradeAcquire(RecipeDB,67130,8,42)
+	if (faction == BFAC["Alliance"]) then
 
-	-- Saronite Swordbreakers (Ally) -- 67092
-	self:addTradeSkill(RecipeDB,67092,450,47570,4,2018,nil,2,450,455,465,475)
-	self:addTradeFlags(RecipeDB,67092,1,6,11,21,22,23,24,25,26,27,28,29,30,36,40,52,59)
-	self:addTradeAcquire(RecipeDB,67092,8,42)
+		-- Breastplate of the White Knight (Ally) -- 67091
+		self:addTradeSkill(RecipeDB,67091,450,47591,4,2018,nil,2,450,455,465,475)
+		self:addTradeFlags(RecipeDB,67091,1,6,11,21,22,23,24,25,26,27,28,29,30,36,40,52,59)
+		self:addTradeAcquire(RecipeDB,67091,8,42)
 
-	-- Saronite Swordbreakers (Horde) -- 67131
-	self:addTradeSkill(RecipeDB,67131,450,47571,4,2018,nil,2,450,455,465,475)
-	self:addTradeFlags(RecipeDB,67131,2,6,11,21,22,23,24,25,26,27,28,29,30,36,40,52,59)
-	self:addTradeAcquire(RecipeDB,67131,8,42)
+		-- Saronite Swordbreakers (Ally) -- 67092
+		self:addTradeSkill(RecipeDB,67092,450,47570,4,2018,nil,2,450,455,465,475)
+		self:addTradeFlags(RecipeDB,67092,1,6,11,21,22,23,24,25,26,27,28,29,30,36,40,52,59)
+		self:addTradeAcquire(RecipeDB,67092,8,42)
 
-	-- Sunforged Bracers (Ally) -- 67096
-	self:addTradeSkill(RecipeDB,67096,450,47574,4,2018,nil,2,450,455,465,475)
-	self:addTradeFlags(RecipeDB,67096,1,6,11,21,22,23,24,25,26,27,28,29,30,36,40,53,59)
-	self:addTradeAcquire(RecipeDB,67096,8,42)
+		-- Sunforged Bracers (Ally) -- 67096
+		self:addTradeSkill(RecipeDB,67096,450,47574,4,2018,nil,2,450,455,465,475)
+		self:addTradeFlags(RecipeDB,67096,1,6,11,21,22,23,24,25,26,27,28,29,30,36,40,53,59)
+		self:addTradeAcquire(RecipeDB,67096,8,42)
 
-	-- Sunforged Bracers (Horde) -- 67135
-	self:addTradeSkill(RecipeDB,67135,450,47575,4,2018,nil,2,450,455,465,475)
-	self:addTradeFlags(RecipeDB,67135,2,6,11,21,22,23,24,25,26,27,28,29,30,36,40,53,59)
-	self:addTradeAcquire(RecipeDB,67135,8,42)
+		-- Sunforged Breastplate (Ally) -- 67095
+		self:addTradeSkill(RecipeDB,67095,450,47593,4,2018,nil,2,450,460,467,475)
+		self:addTradeFlags(RecipeDB,67095,1,6,11,21,22,23,24,25,26,27,28,29,30,36,40,53,59)
+		self:addTradeAcquire(RecipeDB,67095,8,42)
 
-	-- Sunforged Breastplate (Ally) -- 67095
-	self:addTradeSkill(RecipeDB,67095,450,47593,4,2018,nil,2,450,460,467,475)
-	self:addTradeFlags(RecipeDB,67095,1,6,11,21,22,23,24,25,26,27,28,29,30,36,40,53,59)
-	self:addTradeAcquire(RecipeDB,67095,8,42)
+		-- Titanium Razorplate (Ally) -- 67093
+		self:addTradeSkill(RecipeDB,67093,450,47589,4,2018,nil,2,450,455,465,475)
+		self:addTradeFlags(RecipeDB,67093,1,6,11,21,22,23,24,25,26,27,28,29,30,36,40,51,59)
+		self:addTradeAcquire(RecipeDB,67093,8,42)
 
-	-- Sunforged Breastplate (Horde) -- 67134
-	self:addTradeSkill(RecipeDB,67134,450,47594,4,2018,nil,2,450,460,467,475)
-	self:addTradeFlags(RecipeDB,67134,2,6,11,21,22,23,24,25,26,27,28,29,30,36,40,53,59)
-	self:addTradeAcquire(RecipeDB,67134,8,42)
+		-- Titanium Spikeguards (Ally) -- 67094
+		self:addTradeSkill(RecipeDB,67094,450,47572,4,2018,nil,2,450,455,465,475)
+		self:addTradeFlags(RecipeDB,67094,1,6,11,21,22,23,24,25,26,27,28,29,30,36,40,51,59)
+		self:addTradeAcquire(RecipeDB,67094,8,42)
 
-    -- Titanium Razorplate (Ally) -- 67093
-	self:addTradeSkill(RecipeDB,67093,450,47589,4,2018,nil,2,450,455,465,475)
-	self:addTradeFlags(RecipeDB,67093,1,6,11,21,22,23,24,25,26,27,28,29,30,36,40,51,59)
-	self:addTradeAcquire(RecipeDB,67093,8,42)
+		factioncount = 6
 
-    -- Titanium Razorplate (Horde) -- 67132
-	self:addTradeSkill(RecipeDB,67132,450,47590,4,2018,nil,2,450,455,465,475)
-	self:addTradeFlags(RecipeDB,67132,2,6,11,21,22,23,24,25,26,27,28,29,30,36,40,51,59)
-	self:addTradeAcquire(RecipeDB,67132,8,42)
+	elseif (faction == BFAC["Horde"]) then
 
-	-- Titanium Spikeguards (Ally) -- 67094
-	self:addTradeSkill(RecipeDB,67094,450,47572,4,2018,nil,2,450,455,465,475)
-	self:addTradeFlags(RecipeDB,67094,1,6,11,21,22,23,24,25,26,27,28,29,30,36,40,51,59)
-	self:addTradeAcquire(RecipeDB,67094,8,42)
+		-- Breastplate of the White Knight (Horde) -- 67130
+		self:addTradeSkill(RecipeDB,67130,450,47592,4,2018,nil,2,450,455,465,475)
+		self:addTradeFlags(RecipeDB,67130,2,6,11,21,22,23,24,25,26,27,28,29,30,36,40,52,59)
+		self:addTradeAcquire(RecipeDB,67130,8,42)
 
-	-- Titanium Spikeguards (Horde) -- 67133
-	self:addTradeSkill(RecipeDB,67133,450,47573,4,2018,nil,2,450,455,465,475)
-	self:addTradeFlags(RecipeDB,67133,2,6,11,21,22,23,24,25,26,27,28,29,30,36,40,51,59)
-	self:addTradeAcquire(RecipeDB,67133,8,42)
+		-- Saronite Swordbreakers (Horde) -- 67131
+		self:addTradeSkill(RecipeDB,67131,450,47571,4,2018,nil,2,450,455,465,475)
+		self:addTradeFlags(RecipeDB,67131,2,6,11,21,22,23,24,25,26,27,28,29,30,36,40,52,59)
+		self:addTradeAcquire(RecipeDB,67131,8,42)
 
-	return 499
+		-- Sunforged Bracers (Horde) -- 67135
+		self:addTradeSkill(RecipeDB,67135,450,47575,4,2018,nil,2,450,455,465,475)
+		self:addTradeFlags(RecipeDB,67135,2,6,11,21,22,23,24,25,26,27,28,29,30,36,40,53,59)
+		self:addTradeAcquire(RecipeDB,67135,8,42)
+
+		-- Sunforged Breastplate (Horde) -- 67134
+		self:addTradeSkill(RecipeDB,67134,450,47594,4,2018,nil,2,450,460,467,475)
+		self:addTradeFlags(RecipeDB,67134,2,6,11,21,22,23,24,25,26,27,28,29,30,36,40,53,59)
+		self:addTradeAcquire(RecipeDB,67134,8,42)
+
+		-- Titanium Razorplate (Horde) -- 67132
+		self:addTradeSkill(RecipeDB,67132,450,47590,4,2018,nil,2,450,455,465,475)
+		self:addTradeFlags(RecipeDB,67132,2,6,11,21,22,23,24,25,26,27,28,29,30,36,40,51,59)
+		self:addTradeAcquire(RecipeDB,67132,8,42)
+
+		-- Titanium Spikeguards (Horde) -- 67133
+		self:addTradeSkill(RecipeDB,67133,450,47573,4,2018,nil,2,450,455,465,475)
+		self:addTradeFlags(RecipeDB,67133,2,6,11,21,22,23,24,25,26,27,28,29,30,36,40,51,59)
+		self:addTradeAcquire(RecipeDB,67133,8,42)
+
+		factioncount = 6
+
+	end
+
+	return 486 + factioncount
 
 end
