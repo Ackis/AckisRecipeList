91555411b80dbe9b09280f43bfc3385e24b72ce1
pompachomp <pompachomp@dummy.example.com>
2009-10-11 19:01:02 +0000

    Fixed an error with scanning the alchemy profession

--- RecipeDB/ARL-Alchemy.lua
+++ RecipeDB/ARL-Alchemy.lua
@@ -30,6 +30,34 @@ local MODNAME		= "Ackis Recipe List"
 local addon		= LibStub("AceAddon-3.0"):GetAddon(MODNAME)
 local L			= LibStub("AceLocale-3.0"):GetLocale(MODNAME)
 
+--------------------------------------------------------------------------------------------------------------------
+-- Item "rarity"
+--------------------------------------------------------------------------------------------------------------------
+local R_COMMON, R_UNCOMMON, R_RARE, R_EPIC, R_LEGENDARY, R_ARTIFACT = 1, 2, 3, 4, 5, 6
+
+--------------------------------------------------------------------------------------------------------------------
+-- Origin
+--------------------------------------------------------------------------------------------------------------------
+local GAME_ORIG, GAME_TBC, GAME_WOTLK = 0, 1, 2
+
+--------------------------------------------------------------------------------------------------------------------
+-- Filter flags
+--------------------------------------------------------------------------------------------------------------------
+local F_ALLIANCE, F_HORDE, F_TRAINER, F_VENDOR, F_INSTANCE, F_RAID = 1, 2, 3, 4, 5, 6
+local F_SEASONAL, F_QUEST, F_PVP, F_WORLD_DROP, F_MOB_DROP, F_DISC = 7, 8, 9, 10, 11, 12
+local F_DK, F_DRUID, F_HUNTER, F_MAGE, F_PALADIN, F_PRIEST, F_SHAMAN, F_ROGUE, F_WARLOCK, F_WARRIOR = 21, 22, 23, 24, 25, 26, 27, 28, 29, 30
+local F_IBOE, F_IBOP, F_IBOA, F_RBOE, F_RBOP, F_RBOA = 36, 37, 38, 40, 41, 42
+local F_DPS, F_TANK, F_HEALER, F_CASTER = 51, 52, 53, 54
+local F_CLOTH, F_LEATHER, F_MAIL, F_PLATE, F_CLOAK, F_TRINKET, F_RING, F_NECK, F_SHIELD = 56, 57, 58, 59, 60, 61, 62, 63, 64
+local F_1H, F_2H, F_AXE, F_SWORD, F_MACE, F_POLEARM, F_DAGGER = 66, 67, 68, 69, 70, 71, 72
+local F_STAFF, F_WAND, F_THROWN, F_BOW, F_XBOW, F_AMMO, F_FIST, F_GUN = 73, 74, 75, 76, 77, 78, 79, 80
+
+--------------------------------------------------------------------------------------------------------------------
+-- Acquire types
+--------------------------------------------------------------------------------------------------------------------
+local A_TRAINER, A_VENDOR, A_MOB, A_QUEST, A_SEASONAL, A_REPUTATION, A_WORLD_DROP, A_CUSTOM = 1, 2, 3, 4, 5, 6, 7, 8
+
+
 local initialized	= false
 
 function addon:InitAlchemy(RecipeDB)
