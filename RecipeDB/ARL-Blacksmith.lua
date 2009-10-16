2f9eb244778357b79a184fbf007e42fa22610640
Ackis <Ackis@dummy.example.com>
2009-10-16 20:17:35 +0000

    Remove unused var.

--- RecipeDB/ARL-Blacksmith.lua
+++ RecipeDB/ARL-Blacksmith.lua
@@ -2651,8 +2651,6 @@ function addon:InitBlacksmithing(RecipeDB)
 	-- We only add the faction specific recipes if the user is part of that faction
 	local BFAC = LibStub("LibBabble-Faction-3.0"):GetLookupTable()
 	local _,faction = UnitFactionGroup("player")
-	-- Counter for how many faction specific recipes we have
-	local factioncount = 0
 
 	if (faction == BFAC["Alliance"]) then
 
