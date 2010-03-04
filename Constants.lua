37317431ae63948058b8df588db6701377533ba7
pompachomp <coolguy@broinitup.com>
2010-03-04 02:02:13 -0500

    Added private.game_version_names table

--- Constants.lua
+++ Constants.lua
@@ -44,6 +44,12 @@ private.item_quality_names = {
 	[6] = "ARTIFACT",
 }
 
+private.game_version_names = {
+	[1] = "ORIG",
+	[2] =  "TBC",
+	[3] = "WOTLK",
+}
+
 private.game_versions = {
 	["ORIG"] = 1,
 	[ "TBC"] = 2,
