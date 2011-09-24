6ce8c97f54534836d86fee8d9d7b0a6b3ff9dc69
mike <mike@broinitup.com>
2011-09-24 12:22:34 -0400

    Datamined the Blacksmithing Supplies vendor: Sumi ... and also added crafteditemid's to those recipes

--- Database/Vendor.lua
+++ Database/Vendor.lua
@@ -102,7 +102,7 @@ function addon:InitVendor()
 	AddVendor(3335,		L["Hagrus"],				BZ["Orgrimmar"],			46.0, 45.9, "Horde")
 	AddVendor(3346,		L["Kithas"],				BZ["Orgrimmar"],			53.3, 48.9, "Horde") -- COMPLETELY UPDATED
 	AddVendor(3348,		L["Kor'geld"],				BZ["Orgrimmar"],			55.23, 45.83, "Horde") -- UPDATED
-	--AddVendor(3356,		L["Sumi"],				BZ["Orgrimmar"],			75.8, 35.2, "Horde")
+	AddVendor(3356,		L["Sumi"],				BZ["Orgrimmar"],			75.8, 35.2, "Horde") -- COMPLETELY UPDATED
 	AddVendor(3364,		L["Borya"],				BZ["Orgrimmar"],			63.0, 51.2, "Horde")
 	AddVendor(3366,		L["Tamar"],				BZ["Orgrimmar"],			60.3, 54.3, "Horde") -- COMPLETELY UPDATED
 	AddVendor(3367,		L["Felika"],				BZ["Orgrimmar"],			60.5, 50.7, "Horde")
