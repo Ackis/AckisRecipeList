13a0c472234a8da6a612b90e25ab8439da6e9692
James D. Callahan III <jcallahan@curse.com>
2012-01-08 10:39:30 -0600

    Added Inscription filter menu panel.

---
+++ Database/Filters/Inscription.lua
@@ -0,0 +1,116 @@
+-------------------------------------------------------------------------------
+-- Localized Lua globals.
+-------------------------------------------------------------------------------
+local _G = getfenv(0)
+
+local pairs = _G.pairs
+
+-------------------------------------------------------------------------------
+-- AddOn namespace.
+-------------------------------------------------------------------------------
+local FOLDER_NAME, private = ...
+
+local LibStub = _G.LibStub
+local addon = LibStub("AceAddon-3.0"):GetAddon(private.addon_name)
+local L = LibStub("AceLocale-3.0"):GetLocale(private.addon_name)
+
+function private:InitializeItemFilters_Inscription(parent_panel)
+	local MainPanel = addon.Frame
+
+	local armor_toggle = _G.CreateFrame("Button", nil, parent_panel)
+	armor_toggle:SetWidth(105)
+	armor_toggle:SetHeight(20)
+	armor_toggle:SetNormalFontObject("QuestTitleFont")
+	armor_toggle:SetHighlightFontObject("QuestTitleFontBlackShadow")
+	armor_toggle:SetText(_G.ARMOR .. ":")
+	armor_toggle:SetPoint("TOP", parent_panel, "TOP", 0, -7)
+	armor_toggle:RegisterForClicks("LeftButtonUp", "RightButtonUp")
+
+	private.SetTooltipScripts(armor_toggle, L["GROUP_TOGGLE_FORMAT"]:format(_G.ARMOR))
+
+	local armor_types = {
+		inscription_off_hand	= { tt = L["FILTER_DESC_FORMAT"]:format(_G.SECONDARYHANDSLOT),	text = _G.SECONDARYHANDSLOT,	row = 1, col = 1 },
+		inscription_relic	= { tt = L["FILTER_DESC_FORMAT"]:format(_G.INVTYPE_RELIC), 	text = _G.INVTYPE_RELIC,	row = 1, col = 2 },
+	}
+
+	armor_toggle:SetScript("OnClick", function(self, button)
+		local toggle = (button == "LeftButton") and true or false
+
+		for item in pairs(armor_types) do
+			addon.db.profile.filters.item[item] = toggle
+			parent_panel[item]:SetChecked(toggle)
+		end
+		MainPanel:UpdateTitle()
+		MainPanel.list_frame:Update(nil, false)
+	end)
+
+	parent_panel.armor_toggle = armor_toggle
+
+	local armor_panel = _G.CreateFrame("Frame", nil, parent_panel)
+	armor_panel:SetHeight(40)
+	armor_panel:SetPoint("TOP", armor_toggle, "BOTTOM")
+	armor_panel:SetPoint("LEFT", parent_panel, "LEFT")
+	armor_panel:SetPoint("RIGHT", parent_panel, "RIGHT")
+
+	private.GenerateCheckBoxes(parent_panel, armor_types, armor_panel)
+
+	for item_type in pairs(armor_types) do
+		MainPanel.filter_menu.value_map[item_type] = {
+			cb = MainPanel.filter_menu.item.items_inscription[item_type],
+			svroot = addon.db.profile.filters.item
+		}
+	end
+
+	-------------------------------------------------------------------------------
+	-- Create the General toggle and CheckButtons
+	-------------------------------------------------------------------------------
+	local general_toggle = _G.CreateFrame("Button", nil, parent_panel)
+	general_toggle:SetWidth(105)
+	general_toggle:SetHeight(20)
+	general_toggle:SetNormalFontObject("QuestTitleFont")
+	general_toggle:SetHighlightFontObject("QuestTitleFontBlackShadow")
+	general_toggle:SetText(_G.GENERAL .. ":")
+	general_toggle:SetPoint("TOP", armor_panel, "BOTTOM", 0, 0)
+	general_toggle:RegisterForClicks("LeftButtonUp", "RightButtonUp")
+
+	private.SetTooltipScripts(general_toggle, L["GROUP_TOGGLE_FORMAT"]:format(_G.GENERAL))
+
+	local general_types = {
+		inscription_created_item	= { tt = L["FILTER_DESC_FORMAT"]:format(_G.NONEQUIPSLOT),	text = _G.NONEQUIPSLOT,		row = 1, col = 1 },
+		inscription_item_enhancement	= { tt = L["FILTER_DESC_FORMAT"]:format(L["Item Enhancement"]),	text = L["Item Enhancement"],	row = 1, col = 2 },
+		inscription_major_glyph		= { tt = L["FILTER_DESC_FORMAT"]:format(_G.MAJOR_GLYPHS), 	text = _G.MAJOR_GLYPHS,		row = 2, col = 1 },
+		inscription_materials		= { tt = L["FILTER_DESC_FORMAT"]:format(L["Materials"]), 	text = L["Materials"],		row = 2, col = 2 },
+		inscription_minor_glyph		= { tt = L["FILTER_DESC_FORMAT"]:format(_G.MINOR_GLYPHS), 	text = _G.MINOR_GLYPHS,		row = 3, col = 1 },
+		inscription_prime_glyph		= { tt = L["FILTER_DESC_FORMAT"]:format(_G.PRIME_GLYPHS), 	text = _G.PRIME_GLYPHS,		row = 3, col = 2 },
+		inscription_scroll		= { tt = L["FILTER_DESC_FORMAT"]:format(L["Scroll"]), 		text = L["Scroll"],		row = 4, col = 1 },
+	}
+
+	general_toggle:SetScript("OnClick", function(self, button)
+		local toggle = (button == "LeftButton") and true or false
+
+		for item in pairs(general_types) do
+			addon.db.profile.filters.item[item] = toggle
+			parent_panel[item]:SetChecked(toggle)
+		end
+		MainPanel:UpdateTitle()
+		MainPanel.list_frame:Update(nil, false)
+	end)
+
+	parent_panel.general_toggle = general_toggle
+
+	local general_panel = _G.CreateFrame("Frame", nil, parent_panel)
+	general_panel:SetHeight(70)
+	general_panel:SetPoint("TOP", general_toggle, "BOTTOM")
+	general_panel:SetPoint("LEFT", parent_panel, "LEFT")
+	general_panel:SetPoint("RIGHT", parent_panel, "RIGHT")
+
+	private.GenerateCheckBoxes(parent_panel, general_types, general_panel)
+
+	for item_type in pairs(general_types) do
+		MainPanel.filter_menu.value_map[item_type] = {
+			cb = MainPanel.filter_menu.item.items_inscription[item_type],
+			svroot = addon.db.profile.filters.item
+		}
+	end
+	self.InitializeItemFilters_Inscription = nil
+end
