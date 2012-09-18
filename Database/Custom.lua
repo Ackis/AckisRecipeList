bbedbd786ebc36e5ec93208ff8e62f7deef4b253
Ressy <resa1983@hotmail.com>
2012-09-18 18:26:49 -0400

    Add custom string, update localization

--- Database/Custom.lua
+++ Database/Custom.lua
@@ -70,6 +70,7 @@ function addon:InitCustom()
 	AddCustom("DRAGONSOUL_RANDOM", Z.DRAGON_SOUL)
 	AddCustom("PREREQ")
 	AddCustom("BLACK_MARKET_AUCTION_HOUSE")
+	AddCustom("MOP_RAIDS_RANDOM")
 
 	self.InitCustom = nil
 end
