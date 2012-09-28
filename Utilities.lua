e3798246dc7fb88bdf5ad233c00fa8b4b02b8561
John Pasula <ackis@shaw.ca>
2012-09-28 10:40:06 -0600

    Modify dump rep to use private list of faction ID's and only print out if those are true.

--- Utilities.lua
+++ Utilities.lua
@@ -250,7 +250,7 @@ do
 		for index = 1, 1500 do
 			local rep_name = _G.GetFactionInfoByID(index)
 
-			if rep_name then
+			if rep_name and private.FACTION_STRINGS[index] then
 				output:AddLine(("[\"%s\"] = _G.GetFactionInfoByID(%d),"):format(rep_name, index))
 			end
 		end
