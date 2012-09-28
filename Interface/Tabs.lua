d3def3a1ec5688ab6b870522d15ac7bbba443a74
John Pasula <ackis@shaw.ca>
2012-09-28 10:19:12 -0600

    We don't use BFAC in tabs.lua so lets get rid of it.

--- Interface/Tabs.lua
+++ Interface/Tabs.lua
@@ -16,7 +16,6 @@ local FOLDER_NAME, private = ...
 local LibStub = _G.LibStub
 local addon	= LibStub("AceAddon-3.0"):GetAddon(private.addon_name)
 local L		= LibStub("AceLocale-3.0"):GetLocale(private.addon_name)
-local BFAC	= LibStub("LibBabble-Faction-3.0"):GetLookupTable()
 
 -------------------------------------------------------------------------------
 -- Constants
