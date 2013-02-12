4bae913e3a3d7b39d750590fcb10fb866857a24e
John Pasula <ackis@shaw.ca>
2013-02-11 18:34:06 -0700

    Felcloth bag -> retired

--- Database/Recipes/Tailoring.lua
+++ Database/Recipes/Tailoring.lua
@@ -1831,7 +1831,7 @@ function addon:InitTailoring()
 	recipe:SetSkillLevels(280, 280, 300, 315, 330)
 	recipe:SetCraftedItem(21341, "BIND_ON_EQUIP")
 	recipe:SetItemFilterType("TAILORING_BAG")
-	recipe:AddFilters(F.ALLIANCE, F.HORDE, F.INSTANCE, F.WARLOCK)
+	recipe:AddFilters(F.ALLIANCE, F.HORDE, F.INSTANCE, F.WARLOCK, F.RETIRED)
 	recipe:AddMobDrop(10503)
 	recipe:AddCustom("SCHOLO_BOOK_SPAWN")
 
