3185007bfeeb17038bc26504905cc28945e8bd8b
Ressy <resa1983@hotmail.com>
2016-10-19 14:27:53 -0400

    Update comments

--- Database/Mob.lua
+++ Database/Mob.lua
@@ -62,7 +62,7 @@ function addon:InitMob()
 	AddMob(50089,	L["Julak-Doom"],		Z.TWILIGHT_HIGHLANDS,				55.0, 11.4) -- Tailoring, Jewelcrafting
 	AddMob(80927,	BN.CORDANA_FELSONG,		Z.VAULT_OF_THE_WARDENS,				0, 0) -- Enchanting, Jewelcrafting
 	AddMob(96028,	BN.WRATH_OF_AZSHARA,		Z.EYE_OF_AZSHARA,				57.7, 53.6) -- Alchemy, Inscription
-	AddMob(98208,	BN.ADVISOR_VANDROS,		Z.THE_ARCWAY,					0, 0) -- Engineering, Jewelcrafting
+	AddMob(98208,	BN.ADVISOR_VANDROS,		Z.THE_ARCWAY,					0, 0) -- Engineering, Leatherworking, Jewelcrafting
 	AddMob(99611,	BN.THE_AMALGAM_OF_SOULS,	Z.BLACK_ROOK_HOLD,				0, 0) -- Jewelcrafting, Leatherworking
 	AddMob(100497,	BN.URSOC,			Z.THE_EMERALD_NIGHTMARE,			0, 0) -- Enchanting, Inscription
 	AddMob(101403,	BN.SHADE_OF_XAVIUS,		Z.DARKHEART_THICKET,				0, 0) -- Enchanting, Jewelcrafting
