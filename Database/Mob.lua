91a3a6169d0f9c5d597f01630a67df42a2703590
Ressy <resa1983@hotmail.com>
2019-01-31 15:36:06 -0500

    Fix MOB ids & removal of empty

--- Database/Mob.lua
+++ Database/Mob.lua
@@ -40,12 +40,11 @@ function addon:InitMob()
 	AddMob(50061,	L["Xariona"],			Z.DEEPHOLM,					57.3, 62.6) -- Tailoring, Jewelcrafting
 	AddMob(50063,	L["Akma'hat"],			Z.ULDUM,					38.0, 60.2) -- Tailoring, Jewelcrafting
 	AddMob(50089,	L["Julak-Doom"],		Z.TWILIGHT_HIGHLANDS,				55.0, 11.4) -- Tailoring, Jewelcrafting
-	AddMob(80927,	BN.CORDANA_FELSONG,		Z.VAULT_OF_THE_WARDENS,				0, 0) -- Enchanting, Jewelcrafting
 	AddMob(91007,	BN.DARGRUL_THE_UNDERKING,	Z.NELTHARIONS_LAIR,				17.0, 70.0) -- Alchemy, Jewelcrafting, Leatherworking
-	AddMob(94923,	BN.LORD_KURTALOS_RAVENCREST,	Z.BLACK_ROOK_HOLD,				0, 0) -- Alchemy, Jewelcrafting
+	AddMob(94923,	BN.LORD_KURTALOS_RAVENCREST,	Z.BLACK_ROOK_HOLD,				0, 0) -- Alchemy, Jewelcrafting, Leatherworking
+	AddMob(95888,	BN.CORDANA_FELSONG,		Z.VAULT_OF_THE_WARDENS,				0, 0) -- Enchanting, Jewelcrafting
 	AddMob(96028,	BN.WRATH_OF_AZSHARA,		Z.EYE_OF_AZSHARA,				57.7, 53.6) -- Alchemy, Inscription
 	AddMob(98208,	BN.ADVISOR_VANDROS,		Z.THE_ARCWAY,					0, 0) -- Engineering, Leatherworking, Jewelcrafting
-	AddMob(99611,	BN.THE_AMALGAM_OF_SOULS,	Z.BLACK_ROOK_HOLD,				0, 0) -- Jewelcrafting, Leatherworking
 	AddMob(100497,	BN.URSOC,			Z.THE_EMERALD_NIGHTMARE,			0, 0) -- Enchanting, Inscription
 	AddMob(101403,	BN.SHADE_OF_XAVIUS,		Z.DARKHEART_THICKET,				0, 0) -- Enchanting, Jewelcrafting
 	AddMob(102387,	BN.SAELORN,			Z.VIOLET_HOLD,					0, 0) -- Engineering, Inscription, Leatherworking, Jewelcrafting
